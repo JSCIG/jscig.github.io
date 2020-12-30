980ae12aec5b6cd83c359835a8dae1fa2145041b
hax <hax@users.noreply.github.com>
2020-12-30 06:54:51 +0000

    deploy: 382f0ab28f7696e9315ba7e51d8f97199781e0f0

---
+++ sw.js
@@ -0,0 +1,51 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts(
+  "https://cdn.jsdelivr.net/npm/workbox-sw@4.3.1/build/workbox-sw.min.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "7ba7555bb547aa8db9f39a040022b08b"
+  },
+  {
+    "url": "index.webmanifest",
+    "revision": "ba06ae43eeff3346b9908c253fe36f6b"
+  },
+  {
+    "url": "source.5d9d68a5.js",
+    "revision": "1503388302bbf75ca8fc7ec0869b0f55"
+  },
+  {
+    "url": "source.aa3d76dc.css",
+    "revision": "c860248be0e989c450371930d47d5047"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.precaching.cleanupOutdatedCaches();
