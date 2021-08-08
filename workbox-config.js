f046d99854b099098e3fdce4982bc8f807496d81
南漂一卒 <shiy2008@gmail.com>
2021-08-08 14:20:00 +0800

    [optimize] upgrade Upsteam packages (#6)

--- workbox-config.js
+++ workbox-config.js
@@ -1,13 +1,10 @@
 module.exports = {
   globDirectory: 'dist/',
-  globPatterns: [
-    '**/*.{html,css,js,json,webmanifest,ico,gif,jpg,jpeg,png,webp}'
-  ],
+  globPatterns: ['**/*.{html,css,js,json,ico,gif,jpg,jpeg,png,webp}'],
   swDest: 'dist/sw.js',
-  importWorkboxFrom: 'disabled',
   importScripts: [
-    'https://cdn.jsdelivr.net/npm/workbox-sw@4.3.1/build/workbox-sw.min.js'
+    'https://cdn.jsdelivr.net/npm/workbox-sw@6.2.2/build/workbox-sw.min.js',
   ],
   clientsClaim: true,
-  cleanupOutdatedCaches: true
+  cleanupOutdatedCaches: true,
 };
